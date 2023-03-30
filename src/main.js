7f6649532ba9ca10d627a1fa3279a1ab0de99b56
Marco <marco@MacBook-Pro-di-Marco.local>
2023-03-30 18:02:10 +0200

    update index, add js animation on hero description - add styles

---
+++ src/main.js
@@ -0,0 +1,43 @@
+function type(mainWrapper, speed) {
+
+    function isSingleNode(element) {
+        return element.childElementCount === 0
+    }
+
+     function typeChild(element, parent) {
+        const tag = document.createElement(element.tagName)
+        tag.classList = element.classList
+        parent.append(tag)
+        var i = 0
+        var timer =  setInterval(()=> {
+            if (i<element.innerHTML.length +1 ) {
+                    tag.innerHTML = element.innerHTML.substring(0,i)
+                    i++
+            } else {
+                clearInterval(timer)
+            }
+        }, speed)
+    } 
+
+    function iterate(wrapper, parent) {
+        wrapper.childNodes.forEach((element, index)=> {
+            if (element.tagName === undefined) {
+                return
+            }
+            setTimeout(()=> {
+                typeChild(element, parent)
+            },1000*index)
+            
+        }
+        )
+    }
+
+    const wrapper = document.createElement("div")
+    wrapper.innerHTML = mainWrapper.innerHTML
+    mainWrapper.innerHTML = "";
+
+    iterate(wrapper, mainWrapper)
+}
+
+const typewriter = document.querySelector(".typewriter")
+type(typewriter,25)
