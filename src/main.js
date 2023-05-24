459eedbe7a3423b098104d54f52f96be9148aae3
Marco Scapin <markoscapin@gmail.com>
2023-05-24 17:50:30 +0200

    customize html and css for new UI

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-// IMPORT STATEMENTS
-import * as type from './type.js'
+// // IMPORT STATEMENTS
+// import * as type from './type.js'
 
-// QUERY SELECTORS
-const typewriter = document.querySelector(".typewriter")
-const nav = document.querySelector('nav')
-const navButton = document.querySelector('#navbar-button')
-let open = false
+// // QUERY SELECTORS
+// const typewriter = document.querySelector(".typewriter")
+// const nav = document.querySelector('nav')
+// const navButton = document.querySelector('#navbar-button')
+// let open = false
 
 
-//LISTENERS
-navButton.addEventListener('click', (e)=> {
-    if (open) {
-        nav.querySelector('ul').style.display = 'none'
-        open = !open
-    } else {
-        nav.querySelector('ul').style.display = 'block'
-        open = !open
-    }
-})
+// //LISTENERS
+// navButton.addEventListener('click', (e)=> {
+//     if (open) {
+//         nav.querySelector('ul').style.display = 'none'
+//         open = !open
+//     } else {
+//         nav.querySelector('ul').style.display = 'block'
+//         open = !open
+//     }
+// })
 
-//ANIMATIONS
-type(typewriter,25)
+// //ANIMATIONS
+// type(typewriter,25)
